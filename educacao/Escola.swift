9f10af776e2c50e6c30c078775f8f5c5f23bcfb6
Anderson Oliveira <andersonoliveira@Raphael-Grosso-MAC-2.local>
2016-10-29 17:10:53 -0400

    First commit

---
+++ educacao/Escola.swift
@@ -0,0 +1,13 @@
+//
+//  Escola.swift
+//  educacao
+//
+//  Created by Anderson Oliveira on 29/10/16.
+//  Copyright © 2016 Anderson Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class Escola: NSObject {
+
+}
