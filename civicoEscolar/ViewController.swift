ae8e9d577c3089e64f0b541025e41eae548fe183
Thiago Vinhote <thiago.esw@gmail.com>
2016-10-31 21:05:29 -0400

    Initial Commit

---
+++ civicoEscolar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  civicoEscolar
+//
+//  Created by Thiago Vinhote on 31/10/16.
+//  Copyright © 2016 Thiago Vinhote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
